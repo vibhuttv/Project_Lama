5fa2f47a2591109b4fca0b9eccf5b07a0a71002b
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-08-18 22:25:08 +0530

    metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Skai Lama Assignment By Aatmik",
-  description: "This is the assignment for Skai Lama",
+  title: "Skai Lama podcast By Aatmik",
+  description: "This is the web app for Skai Lama",
 };
 
 export default function RootLayout({ children }) {
