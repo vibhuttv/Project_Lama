e30b47fc1ed6fcf89b5e16f92c8b2d69b3b2150f
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-08-18 22:23:59 +0530

    icon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
